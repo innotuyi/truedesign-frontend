ce0f8d7fc4e88262c7162cb7df2d09bcf0aa33b6
innotuy <innotuyi67@gmail.com>
2023-12-15 10:59:42 +0200

    chaning static url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
     apiUrl: "https://phplaravel-1182995-4153897.cloudwaysapps.com",
-    imgUrl:"https://phplaravel-1182995-4153897.cloudwaysapps.com/"
+    imgUrl:"https://phplaravel-1182995-4153897.cloudwaysapps.com/storage"
   };
   
   export default config;
