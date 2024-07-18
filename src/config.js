9cf34613a8b1bd75c5d949c397ce215b9e202beb
TUYISHIMIRE Innocent <innocent.tuyishimire@parsenate.local>
2024-07-18 22:16:45 +0200

    updating configuration file

--- src/config.js
+++ src/config.js
@@ -1,9 +1,20 @@
 // src/config.js
 
-const config = {
-    apiUrl: "http://127.0.0.1:8000",
-    imgUrl:"http://127.0.0.1:8000/storage/"
-  };
+// const config = {
+//     apiUrl: "http://127.0.0.1:8000",
+//     imgUrl:"http://127.0.0.1:8000/storage/"
+//   };
   
-  export default config;
+// export default config;
+
+
+  //src/config.js
+
+const config = {
+  apiUrl: "https://test.amamaza.rw/",
+  imgUrl:"https://test.amamaza.rw/storage/"
+};
+
+export default config;
+
   
